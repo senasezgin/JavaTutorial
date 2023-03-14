0a61ecd169a11344d858130d7b2516de01bd4b64
senasezgin <senaszginn@gmail.com>
2023-03-14 18:25:37 +0300

    .

--- Armstrong.java
+++ Armstrong.java
@@ -4,37 +4,26 @@ import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Sayı Giriniz :");
-        int number = input.nextInt();
-        int basNumber = 0;
-        int tempNumber = number;
-        int basValue;
+        Scanner myScanner = new Scanner(System.in);
+
+        // Get a number from user.
+
+        System.out.print("Bir sayı giriniz : ");
+        int number = myScanner.nextInt();
+
         int result = 0;
-        int basPow;
 
-        while (tempNumber != 0) {
-            tempNumber /= 10;
-            basNumber++;
+        // Calculate the sum of digits.
+
+        result = number % 10;
+
+        while (number != 0) {
+            number /= 10;
+            result += number % 10;
         }
 
-        tempNumber = number;
-        while (tempNumber != 0) {
-            basValue = tempNumber % 10;
-            // 1*1*1*1 = 1^4
-            basPow = 1;
-            for (int i = 1; i <= basNumber; i++) {
-                basPow *= basValue;
-            }
-            result += basPow;
-            tempNumber /= 10;
-        }
+        System.out.println("Girdiğiniz sayının basamakları toplamı : " + result);
 
-        if (result == number) {
-            System.out.println(number + " sayısı bir Armstrong sayıdır.");
-        } else {
-            System.out.println(number + " sayısı bir Armstrong sayısı değildir.");
-        }
 
 
         /*
