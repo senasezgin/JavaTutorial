ef7ff5272a2b43d8070287c67a28719cc5d3ae3e
senasezgin <senaszginn@gmail.com>
2023-03-14 10:27:28 +0300

    .

--- UcDortKatları.java
+++ UcDortKatları.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class UcDortKatları {
     public static void main(String[] args){
-        int k;
+        int k, sayi=0 , toplam=0 ;
         Scanner inp = new Scanner(System.in);
 
         System.out.println("Sayı giriniz: ");
         k= inp.nextInt();
 
         for (int i=0; i <=k; i++){
-            if(i%3 ==0 && i%4==0 ){
-                System.out.println(i);
+            if((i%3 ==0) && (i%4==0) ){
+                sayi +=1;
+                toplam +=i;
+
             }
-        }
+        } System.out.println(" 3 ve 4'ün katlarının ortalaması: " + (toplam/sayi));
     }
 
 }
