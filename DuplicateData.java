0bccd9bdc5ecac490913406e3e358f14b4b5769a
senasezgin <senaszginn@gmail.com>
2023-03-30 17:22:11 +0300

    .

--- DuplicateData.java
+++ DuplicateData.java
@@ -3,36 +3,29 @@ package JavaTutorial;
 import java.util.Arrays;
 
 public class DuplicateData {
-    static boolean isFind(int[] arr, int value) {
-        for (int i : arr) {
-            if (i == value) {
-                return true;
+    static boolean kontrol(int[] dizi ,  int value ){
+        for(int i : dizi){
+            if (value ==  i ){
+                return false;
             }
         }
-        return false;
-
+        return  true;
     }
-
     public static void main(String[] args) {
-        int[] list = {1, 2, 3, 4, 3,4,6,6, 2, 9, 20};
-        int[] dublicate = new int[list.length];
-        int startIndex = 0;
-        for (int i = 0; i < list.length; i++) {
-            for (int j = 0; j < list.length; j++) {
-                if ((i != j) && (list[i] == list[j])) {
-                    if (dublicate[i] % 2 == 0) {
-                        dublicate[startIndex++] = list[i];
-
+        int[] dizi  = {1,2,123,1,23,2,32,123,1};
+        int[] tkr  = new int[dizi.length];
+        int sayac  = 0;
+        for (int i = 0 ; i< dizi.length;i++){
+            for(int j = 0 ; j < dizi.length;j++){
+                if (i != j && dizi[i]== dizi[j]){
+                    if(kontrol(tkr,dizi[i]) && (dizi[i]%2 == 0)){
+                        tkr[sayac++] = dizi[i];
                     }
+                    break;
                 }
-                for (int value : dublicate) {
-                    if (value != 0) {
-                        System.out.println(value);
 
-                    }
-
-                }
             }
         }
+        System.out.println(Arrays.toString(tkr));
     }
 }
