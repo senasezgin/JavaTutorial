4676ae3a1e8abb834dfb04db30421010b0ee0dc7
senasezgin <senaszginn@gmail.com>
2023-04-24 18:25:07 +0300

    .

--- ArtikYil.java
+++ ArtikYil.java
@@ -1,4 +1,26 @@
 package JavaTutorial;
 
+import java.util.Scanner;
+
 public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        double m = input.nextDouble();
+        input.nextLine();
+        String s =
+        System.out.println("String : " +s);
+        System.out.println("Double : " + m);
+        System.out.println("Int : " + n);
+    }
+
+    }
+
+
+
+
 }
+
+
+
+
--- Harmonik.java
+++ Harmonik.java
@@ -7,6 +7,7 @@ public class Harmonik {
         Scanner input = new Scanner(System.in);
         System.out.print("n sayısını giriniz: ");
         int n = input.nextInt();
+
         double result = 0;
         for(int i=1; i<=n; i++){
             result +=(1.0/i);
