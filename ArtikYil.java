fc798ca64c205d3de1907d9a9118db4127a13080
senasezgin <senaszginn@gmail.com>
2023-05-09 10:13:42 +0300

    .

--- ArtikYil.java
+++ ArtikYil.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 public class ArtikYil {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        double m = input.nextDouble();
-        input.nextLine();
-        String s =
-        System.out.println("String : " +s);
-        System.out.println("Double : " + m);
-        System.out.println("Int : " + n);
-    }
+
 
     }
 
